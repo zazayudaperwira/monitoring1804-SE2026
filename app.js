cecf463b6244f429889d17e3d4d9f3036db70a1a
zazayudaperwira <52827975+zazayudaperwira@users.noreply.github.com>
2026-06-24 15:41:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
+
 const API="https://script.google.com/macros/s/AKfycbw5gYyAM9v5JxW_70_TeBOyGB1yIAfqixzUgUp98BXPG50LNNQdz9Pr5uHrk_pXzRy4-A/exec";
 
 let allData={};
 let chart=null;
 
 const STATUS_COLS=[
-"OPEN",
-"DRAFT",
-"SUBMITTED BY Pencacah",
-"REJECTED BY Pengawas",
-"APPROVED BY Pengawas",
-"REVOKED BY Pengawas",
-"SUBMITTED RESPONDENT",
+"OPEN","DRAFT","SUBMITTED BY Pencacah",
+"REJECTED BY Pengawas","APPROVED BY Pengawas",
+"REVOKED BY Pengawas","SUBMITTED RESPONDENT",
 "EDITED BY Pengawas"
 ];
 
@@ -38,27 +35,30 @@ updateChart();
 
 
 
+function progressValue(x){
+
+return Number(
+x.PROGRES ??
+x["Persentase Progres"] ??
+0
+);
+
+}
+
+
+
 function updateKPI(){
-
-let sls=allData.SLS||[];
-
-$("#totalSLS").text(sls.length);
-
-let done=sls.reduce((a,b)=>a+Number(b["APPROVED BY Pengawas"]||0),0);
-
-$("#totalDone").text(done);
-
-if(allData.Kecamatan){
 
 let d=allData.Kecamatan.find(x=>x.Kecamatan==="Lampung Timur");
 
 if(d){
-let p=Number(d.PROGRES)*100;
+
+let p=progressValue(d)*100;
+
 $("#kabProgres").text(p.toFixed(1)+"%");
-$("#progressCard").text(p.toFixed(1)+"%");
 }
 
-}
+$("#totalSLS").text(allData.SLS.length);
 
 }
 
@@ -69,7 +69,7 @@ function loadFilter(){
 [...new Set(allData.Kecamatan.map(x=>x.Kecamatan))]
 .forEach(k=>{
 
-$("#fKec").append(`<option>${k}</option>`)
+$("#fKec").append(`<option value="${k}">${k}</option>`)
 
 });
 
@@ -83,14 +83,14 @@ $("#fDesa").html('<option value="">Semua Desa</option>');
 
 allData.Desa
 .filter(x=>x.Kecamatan==$(this).val())
-.forEach(d=>{
+.forEach(x=>{
 
-$("#fDesa").append(`<option>${d.Desa}</option>`)
+$("#fDesa").append(`<option value="${x.Desa}">${x.Desa}</option>`)
 
 });
 
-updateChart();
 
+updateChart();
 applyFilters();
 
 });
@@ -105,14 +105,82 @@ applyFilters();
 
 
 
+
+function sortProgress(arr){
+
+return [...arr].sort((a,b)=>
+progressValue(b)-progressValue(a)
+);
+
+}
+
+
+
+function updateLeaderboard(){
+
+
+renderRank(sortProgress(allData.Kecamatan),"topKec");
+
+renderRank(sortProgress(allData.Desa),"topDesa");
+
+
+let ppl=sortProgress(allData.PETUGAS);
+
+
+renderRank(ppl,"topPetugas",true);
+
+renderRank(
+[...ppl].reverse(),
+"bottomPetugas",
+true
+);
+
+
+}
+
+
+
+function renderRank(data,id,isPPL=false){
+
+
+$("#"+id).html(
+
+data.slice(0,10)
+.map((x,i)=>{
+
+let nama=x.PPL||x.Desa||x.Kecamatan;
+
+let p=(progressValue(x)*100).toFixed(1);
+
+return `
+<div class="border-b py-2">
+<b>${i+1}</b> ${nama}
+<br>
+<span class="text-xs text-gray-500">
+Progress ${p}%
+</span>
+</div>
+`
+
+})
+.join("")
+
+)
+
+}
+
+
+
+
 function updateChart(){
 
 let kec=$("#fKec").val();
 let desa=$("#fDesa").val();
 
 let data=allData.SLS.filter(x=>
-(!kec||x.Kecamatan==kec)&&
-(!desa||x.Desa==desa)
+(!kec || x.Kecamatan==kec)
+&&
+(!desa || x.Desa==desa)
 );
 
 
@@ -123,7 +191,7 @@ data:[]
 }));
 
 
-if(kec=="" && desa==""){
+if(!kec){
 
 labels=["Kabupaten"];
 
@@ -135,14 +203,16 @@ data.reduce((a,b)=>a+Number(b[ds.label]||0),0)
 
 }else{
 
+
 labels=[...new Set(data.map(x=>x.Desa))];
+
 
 datasets.forEach(ds=>{
 
 ds.data=labels.map(l=>
 data.filter(x=>x.Desa==l)
 .reduce((a,b)=>a+Number(b[ds.label]||0),0)
-)
+);
 
 });
 
@@ -150,13 +220,20 @@ data.filter(x=>x.Desa==l)
 
 
 
-if(chart)chart.destroy();
+if(chart) chart.destroy();
+
 
 chart=new Chart(
 document.getElementById("progresChart"),
 {
+
 type:"bar",
-data:{labels,datasets},
+
+data:{
+labels,
+datasets
+},
+
 options:{
 responsive:true,
 maintainAspectRatio:false,
@@ -165,53 +242,32 @@ x:{stacked:true},
 y:{stacked:true}
 }
 }
-}
-);
+
+});
 
 }
 
-
-
-function updateLeaderboard(){
-
-renderRank(allData.Kecamatan,"topKec");
-renderRank(allData.Desa,"topDesa");
-
-let ppl=[...allData.PETUGAS]
-.sort((a,b)=>Number(b["Persentase Progres"])-Number(a["Persentase Progres"]));
-
-renderRank(ppl,"topPetugas");
-
-renderRank(ppl.slice().reverse(),"bottomPetugas");
-
-}
-
-
-
-function renderRank(data,id){
-
-$("#"+id).html(
-
-data.slice(0,10).map((x,i)=>`
-
-<div class="border-b py-2">
-<b>${i+1}</b>
-${x.PPL||x.Desa||x.Kecamatan}
-</div>
-
-`).join("")
-
-);
-
-}
 
 
 
 function switchTab(sheet){
 
 if($.fn.DataTable.isDataTable("#mainTable")){
+
 $("#mainTable").DataTable().destroy();
+
 }
+
+
+let btn=`button[onclick="switchTab('${sheet}')"]`;
+
+$(".tab-btn")
+.removeClass("bg-orange-600 text-white");
+
+
+$(btn)
+.addClass("bg-orange-600 text-white");
+
 
 
 $("#mainTable").DataTable({
@@ -220,11 +276,25 @@ data:allData[sheet],
 
 columns:Object.keys(allData[sheet][0])
 .map(k=>({
+
 title:k,
-data:k
+data:k,
+
+render:function(data){
+
+if(k.toLowerCase().includes("progres"))
+
+return (Number(data)*100).toFixed(1)+"%";
+
+return data;
+
+}
+
 })),
 
-scrollX:true
+scrollX:true,
+
+destroy:true
 
 });
 
@@ -248,6 +318,7 @@ $("#fDesa").val()
 }
 
 }
+
 
 
 
